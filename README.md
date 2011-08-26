789ac1a05dda218b01c6226665f8a7d94240f75b
David E. Wheeler <david@justatheory.com>
2011-08-26 10:16:10 -0700

    Increment to v0.10.1.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-PGXN/API/Search v0.10.0
+PGXN/API/Search v0.10.1
 ======================
 
 This library's module, PGXN::API::Searcher, provides an interface to the
--- lib/PGXN/API/Searcher.pm
+++ lib/PGXN/API/Searcher.pm
@@ -8,7 +8,7 @@ use Lucy::Search::IndexSearcher;
 use Lucy::Highlight::Highlighter;
 use Carp;
 
-our $VERSION = v0.10.0;
+our $VERSION = v0.10.1;
 
 sub new {
     my ($class, $path) = @_;
